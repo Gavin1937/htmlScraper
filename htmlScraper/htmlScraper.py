b830765be311d7a577e696a2d6197f2922bfe7dd
Gavin1937 <gyh1837771475gyh@gmail.com>
2022-11-19 03:47:36 -0800

    add getOneStr

--- htmlScraper/htmlScraper.py
+++ htmlScraper/htmlScraper.py
@@ -59,4 +59,31 @@ class htmlScraper:
         
         return ret
     
-    pass
+    def getOneStr(self, selector:str) -> str:
+        """
+        Make http get request with self.url and extract text from one element from received content.
+        
+        Parameter:
+            selector: string css selector to extract one html element
+        
+        Returns:
+            if success, return string in the html element selected
+            if failed, return None
+        """
+        
+        url = self.url
+        ret:Tag = None
+        try:
+            res = requests.get(url, headers=HEADERS)
+            if res.ok:
+                soup:BeautifulSoup = BeautifulSoup(res.content.decode('utf-8'), 'lxml')
+            else:
+                raise RequestFailed(f'Failed to request url = {url}')
+            
+            ret = soup.select_one(selector)
+        except Exception as e:
+            raise
+        
+        return ret.getText()
+    
+    
